c720b241b86dbc363d7c11afae4a692b189bd9bb
MB <marcos.abf@gmail.com>
2017-04-03 16:58:40 -0300

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
 import { MensagemModule } from './mensagem/mensagem.module';
+
+// Must export the config
+export const firebaseConfig = {
+    apiKey: "AIzaSyDKOlswDHl6fn4KeSPwyRMA5kWSNq42MeQ",
+    authDomain: "angularestudo-12c45.firebaseapp.com",
+    databaseURL: "https://angularestudo-12c45.firebaseio.com",
+    storageBucket: "angularestudo-12c45.appspot.com",
+    messagingSenderId: "206178587178"
+};
 
 @NgModule({
   declarations: [
@@ -14,6 +24,7 @@ import { MensagemModule } from './mensagem/mensagem.module';
     BrowserModule,
     FormsModule,
     HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     MensagemModule
   ],
   providers: [],
